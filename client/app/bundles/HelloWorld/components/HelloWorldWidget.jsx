6892b382fa7a28bdffc2a55f4f77927123bb1faf
patrice prosper <new>
2017-02-21 12:57:11 -0500

    working app

--- client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
@@ -1,11 +1,15 @@
-//HelloWorldWidget is an arbitrary name for any "dumb" component
+// HelloWorldWidget is an arbitrary name for any "dumb" component. We do not recommend suffixing
+// all your dump component names with Widget.
+
 import React, { PropTypes } from 'react';
 
-
-export default class HelloWorld extends React.Component {
+// Simple example of a React "dumb" component
+export default class HelloWorldWidget extends React.Component {
   static propTypes = {
-    updateName: PropTypes.fun.isRequired
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
+    // If you have lots of data or action properties, you should consider grouping them by
+    // passing two properties: "data" and "actions".
+    updateName: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
   };
 
   // React will automatically provide us with the event `e`
@@ -14,25 +18,25 @@ export default class HelloWorld extends React.Component {
     this.props.updateName(name);
   }
 
-    render() {
-      const { name } = this.props;
-      return (
-        <div className="container">
-          <h3>
-            Hello, {name}!
-          </h3>
-          <hr />
-          <form className="form-horizontal">
-            <label>
-              Say hello to:
-            </label>
-            <input
-              type="text"
-              value={name}
-              onChange={e => this.handleChange(e)}
-            />
-          </form>
-        </div>
-      );
-    }
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="container">
+        <h3>
+          Hello, {name}!
+        </h3>
+        <hr />
+        <form className="form-horizontal">
+          <label>
+            Say hello to:
+          </label>
+          <input
+            type="text"
+            value={name}
+            onChange={e => this.handleChange(e)}
+          />
+        </form>
+      </div>
+    );
   }
+}
